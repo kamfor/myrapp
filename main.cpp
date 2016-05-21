9a7bca9e01710e31bca707d9088a0ed42f02aa44
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-05-21 14:43:43 +0300

    moving

--- main.cpp
+++ main.cpp
@@ -16,15 +16,6 @@ int main(int argc, char * argv[]) {
     std::this_thread::sleep_for(std::chrono::seconds(2));
     talk_to_me(argc, argv);
 
-    rapp::robot::navigation nav;
-    bool status;
-    status = nav.lookAtPoint(10,1,1);
-    status = nav.moveTo(1, 0, 0);
-    status = nav.lookAtPoint(10,2,0);
-    status = nav.moveTo(0, 1, 5);
-    status = nav.lookAtPoint(10,0,2);
-    status = nav.moveTo(1, 1, 5);
-
 
     return 0;
 }
--- src/camera_movement.cpp
+++ src/camera_movement.cpp
@@ -17,7 +17,7 @@ int camera_movement() {
     joint_names.push_back("head_yaw");
     joint_angles.push_back(-1); // turn head in yaw to 1.57 rad orientation
 
-    nav.moveVel(1, 8);
+    //nav.moveVel(1, 8);
     for(int i = 0; i<10; ++i) {
         nav.moveJoint(joint_names, joint_angles);
         std::this_thread::sleep_for(std::chrono::seconds(2));
