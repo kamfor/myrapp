8efce5a84ab7137385acb78e3d026af42802f58f
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-05-21 15:30:30 +0300

    moving

--- src/camera_movement.cpp
+++ src/camera_movement.cpp
@@ -29,12 +29,12 @@ int camera_movement() {
     joint_names.push_back("head_pitch");
     joint_angles.push_back(0); // turn head in pitch to -1 rad orientation
     joint_names.push_back("head_yaw");
-    joint_angles.push_back(2); // turn head in yaw to 1.57 rad orientation
+    joint_angles.push_back(1.7); // turn head in yaw to 1.57 rad orientation
 
     joint_names2.push_back("head_pitch");
     joint_angles2.push_back(0); // turn head in pitch to -1 rad orientation
     joint_names2.push_back("head_yaw");
-    joint_angles2.push_back(1.4); // turn head in yaw to 1.57 rad orientation
+    joint_angles2.push_back(1.2); // turn head in yaw to 1.57 rad orientation
 
     nav.moveVel(1, 8);
     for(int i = 0; i<5; ++i) {
