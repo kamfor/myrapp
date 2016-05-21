377f06e5f37a50031ef96bf8bd160f9700489987
kamfor <kfforex@gmail.com>
2016-05-21 11:23:56 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <rapp-robots-api/info/info.hpp>
+#include <rapp-robots-api/localization/localization.hpp>
+
+
+int main(int argc, char * argv[]) {
+    rapp::robot::info info(argc, argv);
+    rapp::robot::localization loc;
+    rapp::object::qr_code_map qr_map;
+    std::ifstream ifstr(info.get_path("share/myrapp/example_qr_map.xml"));
+    qr_map = loc.load_qr_code_map(ifstr);
+    std::cout<<qr_map.labels.at(0)<<std::endl;
+    std::cout<<qr_map.labels.at(3)<<std::endl;
+    return 0;
+}
