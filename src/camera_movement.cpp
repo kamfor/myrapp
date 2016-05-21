43adff8b6fdcd53721e82e57ab639f851694f29a
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-05-21 15:25:49 +0300

    moving

--- src/camera_movement.cpp
+++ src/camera_movement.cpp
@@ -2,7 +2,7 @@
 
 int camera_movement() {
     rapp::robot::navigation nav;
-
+/*
     std::vector<std::string> joint_names3;
     joint_names3.clear();
     std::vector<float> joint_angles3;
@@ -14,7 +14,7 @@ int camera_movement() {
     joint_angles3.push_back(0.5); // turn head in yaw to 1.57 rad orientation
     nav.moveJoint(joint_names3, joint_angles3);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+*/
 
     std::vector<std::string> joint_names;
     joint_names.clear();
@@ -29,12 +29,12 @@ int camera_movement() {
     joint_names.push_back("head_pitch");
     joint_angles.push_back(0); // turn head in pitch to -1 rad orientation
     joint_names.push_back("head_yaw");
-    joint_angles.push_back(0.3); // turn head in yaw to 1.57 rad orientation
+    joint_angles.push_back(-0.8); // turn head in yaw to 1.57 rad orientation
 
     joint_names2.push_back("head_pitch");
     joint_angles2.push_back(0); // turn head in pitch to -1 rad orientation
     joint_names2.push_back("head_yaw");
-    joint_angles2.push_back(-0.3); // turn head in yaw to 1.57 rad orientation
+    joint_angles2.push_back(-0.8); // turn head in yaw to 1.57 rad orientation
 
     nav.moveVel(1, 8);
     for(int i = 0; i<5; ++i) {
