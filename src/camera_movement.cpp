59f36424980a6817413959aeaa3543d1d00ecf7b
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-05-21 15:19:13 +0300

    moving

--- src/camera_movement.cpp
+++ src/camera_movement.cpp
@@ -13,22 +13,22 @@ int camera_movement() {
     joint_angles2.clear();
 
     joint_names.push_back("head_pitch");
-    joint_angles.push_back(-0.5); // turn head in pitch to -1 rad orientation
+    joint_angles.push_back(0); // turn head in pitch to -1 rad orientation
     joint_names.push_back("head_yaw");
     joint_angles.push_back(0.3); // turn head in yaw to 1.57 rad orientation
 
     joint_names2.push_back("head_pitch");
-    joint_angles2.push_back(0.5); // turn head in pitch to -1 rad orientation
+    joint_angles2.push_back(0); // turn head in pitch to -1 rad orientation
     joint_names2.push_back("head_yaw");
     joint_angles2.push_back(-0.3); // turn head in yaw to 1.57 rad orientation
 
     //nav.moveVel(1, 8);
-    for(int i = 0; i<4; ++i) {
+    for(int i = 0; i<5; ++i) {
         //nav.lookAtPoint(0,0,0);
         nav.moveJoint(joint_names, joint_angles);
-        std::this_thread::sleep_for(std::chrono::seconds(4));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
         nav.moveJoint(joint_names2, joint_angles2);
-        std::this_thread::sleep_for(std::chrono::seconds(4));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
     //nav.moveStop();
     return 0;
