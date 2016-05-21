a25a1e0d46e8e308c19b0c9420bc1a2eab6db338
kamfor <kfforex@gmail.com>
2016-05-21 14:14:46 +0200

    kasdk

--- src/talk_to_me.cpp
+++ src/talk_to_me.cpp
@@ -4,12 +4,7 @@ int talk_to_me(int argc, char * argv[]) {
     rapp::robot::info info(argc, argv);
     rapp::robot::communication comm(argc, argv);
 
-    comm.text_to_speech("I will kill you");
-
-    comm.text_to_speech("I will kill you");
-
-    comm.text_to_speech("I will kill you");
-
+    comm.text_to_speech("         I will kill you         ");
 
     return 0;
 }
